a64d703a062a3723f5cfd4a4499718add9d3ac44
a329336057 <329336057@qq.com>
2019-09-26 16:59:28 +0800

    优化部分修改的新版内容

---
+++ app/src/main/java/com/qifeixianapp/qfxdemo/CellView/MyMarkerView.java
@@ -0,0 +1,46 @@
+package com.qifeixianapp.qfxdemo.CellView;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.data.CandleEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.utils.MPPointF;
+import com.github.mikephil.charting.utils.Utils;
+import com.qifeixianapp.qfxdemo.R;
+
+public class MyMarkerView extends MarkerView {
+
+    private TextView tvContent;
+
+    public MyMarkerView(Context context, int layoutResource) {
+        super(context, layoutResource);
+
+        tvContent= (TextView) findViewById(R.id.tvContent);
+    }
+
+    @Override
+    public void refreshContent(Entry e, Highlight highlight) {
+
+        if (e instanceof CandleEntry) {
+
+            CandleEntry ce = (CandleEntry) e;
+
+            tvContent.setText("" + Utils.formatNumber(ce.getHigh(), 0, true));
+        } else {
+
+            tvContent.setText("" + Utils.formatNumber(e.getY(), 0, true));
+        }
+
+        super.refreshContent(e, highlight);
+    }
+
+    @Override
+    public MPPointF getOffset() {
+        return new MPPointF(-(getWidth() / 2), -getHeight());
+    }
+}
